b6c504d4f78a0b6d4d7f4b6dc0a258b68951e793
Micah Morgan <mnmorgan@ou.edu>
2011-02-23 16:49:54 -0600

    M time: 5 comment: just added a few comments to the IapngExploder.lisp

--- code/interfaces/IapngExploder.lisp
+++ code/interfaces/IapngExploder.lisp
@@ -7,10 +7,22 @@
 
 (interface IapngExploder
   
+  ;; Main function for breaking an APNG file into separate PNGs
+  ;; apnginput is the string of the apng file data?
+  ;; apngxml is the fCTL/aCTL data already parsed out?
+  ;; pngnamebase is the name of the APNG file which will be used for the PNGs
+  ;; state is state
+  ;; Output is a list of mvs: (file-data, file-name)
   (sig explodeAPNG (apnginput, apngxml, pngnamebase, state))
   
+
+  ;; This function pulls out one IDAT or FDAT chunk from the APNG file
+  ;; apngfiledata is the string of the APNG's file data (how odd)
+  ;; Output is a single frame's DAT as a string
   (sig chopFrame (apngfiledata))
   
+
+  ;; This makes a full PNG file string out of a DAT chunk?
   (sig makePNG (framedata))
   
   )
