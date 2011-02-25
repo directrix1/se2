99121f8fcb605818e39e3c3d90a49ee0ca9db111
Zach Bartlett <zachary.d.bartlett-1@ou.edu>
2011-02-24 18:22:59 -0600

    Z time: 40 comment: wrote tests for IpngUtils - need to talk with group about makeNum and parseNum tests

--- code/interfaces/IpngUtils.lisp
+++ code/interfaces/IpngUtils.lisp
@@ -4,4 +4,24 @@
   (sig makeNum (num flag))
   (sig parseNum (string flag))
   (sig calcCRC32 (string))
+
+  (con blowChunks-returns-at-least-IHDR-IDAT
+	(implies (stringp pngdat)
+		 (let ((chunksBlown (blowChunks pngdat)))
+		 	(and (eql (caar chunksBlown) "IHDR")
+			   (or (eql (second (car chunksBlown) "IDAT"))
+			      (eql (fourth (car chunksblown) "IDAT")))))))
+
+ (con makeChunk-returns-string
+	(implies (and (stringp chunktype)
+		      (stringp chunkdat)
+		(let ((chunked (makeChunk (chunktype chunkdat))))
+		     (stringp chunked)))))
+
+ (con makeNum-returns-unsigned-int
+	(implies (acl2-numberp number)
+		 (acl2-numberp flag)
+		(let ((uns-int (makeNum number flag)))
+		     ;something here to test - no idea
+		)))
 )
