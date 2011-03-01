17e25a99a4b787449f9d0e41b89c0a112fd89849
Kye <kyerabon@ou.edu>
2011-03-01 12:54:22 -0600

    K time: 5 comment: Wrote a couple of cons for XMLUtils

--- code/interfaces/IxmlUtils.lisp
+++ code/interfaces/IxmlUtils.lisp
@@ -1,6 +1,15 @@
-(interface IpngUtils
+(interface IxmlUtils
   (sig parseXML (domXML))
   (sig getFrames (framelist))
   (sig writeXML (numPlays numFrames framedata))
   (sig writeFrames (frameData))
+
+  (con parseXML-returns-three
+	(implies (true-listp domXML)
+		 (= (len (parseXML domXML)) 3)))
+
+  (con writeXML-delivers-string
+	(implies (and (and (stringp numPlays) (stringp numFrames))
+		(true-lisp framedata)
+			(stringp (writeXML numPlays numFrames framedata))))
 )
