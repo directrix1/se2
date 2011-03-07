9bb8510a89bf9231641f21807605d2e28812c2d7
Zach Bartlett <zachary.d.bartlett-1@ou.edu>
2011-03-07 17:21:05 -0600

    Z time: 20 comment: updated contracts in Iapngbuilder and IxmlUtils

--- code/interfaces/Iio.lisp
+++ code/interfaces/Iio.lisp
@@ -11,19 +11,23 @@
 
 (interface Iio
 
-  ;Writes an animated portable network graphic file to disk. 
-  ;xmlfilename = (string) the name of the XML document containing information on number of frames, number of plays, PNG filenames, 
-  ;and the length of time each frame is displayed.
+  ; Writes an animated portable network graphic file to disk. 
+  ; xmlfilename = (string) the name of the XML document containing
+  ; information on number of frames, number of plays, PNG filenames, 
+  ; and the length of time each frame is displayed.
   (sig animate (xmlfilename, state))
 
-  ;Writes a series of portable network graphic files to disk, along with an XML document. These image files are the individual frames 
-  ;of the APNG file in the parameter, and the XML document contains information on the number of frames, number of plays, PNG filenames, 
-  ;and length of time each frame is displayed.
-  ;apngfilename = (string) the name of the animated portable network graphic to be broken into individual frames.
+  ; Writes a series of portable network graphic files to disk, along 
+  ; with an XML document. These image files are the individual frames 
+  ; of the APNG file in the parameter, and the XML document contains
+  ; information on the number of frames, number of plays, PNG filenames, 
+  ; and length of time each frame is displayed.
+  ;apngfilename = (string) the name of the animated portable network 
+  ;    graphic to be broken into individual frames.
   (sig suspend (apngfilename, state))
 
-  ;Helper function for suspend. Writes PNG files to disk.
-  ;filelist = list of mv (filename filedata).
+  ; Helper function for suspend. Writes PNG files to disk.
+  ; filelist = list of mv (filename filedata).
   (sig writeFiles (filelist, state))
 
   
