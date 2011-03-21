7f676911444ed7698b019ae2a227ca3bfa7c8798
Zach Bartlett <zachary.d.bartlett-1@ou.edu>
2011-03-21 16:56:41 -0500

    Z time: 10 comment: fixed IapngUtils header2

--- code/interfaces/IapngBuilder.lisp
+++ code/interfaces/IapngBuilder.lisp
@@ -1,4 +1,7 @@
-(in-package "ACL2")
+;; The first four lines of this file were added by Dracula.
+;; They tell DrScheme that this is a Dracula Modular ACL2 program.
+;; Leave these lines unchanged so that DrScheme can properly load this file.
+#reader(planet "reader.rkt" ("cce" "dracula.plt") "modular" "lang")(in-package "ACL2")
 
 (interface IapngBuilder
   ; Returns an APNG as a string that has the following playtime properties:
