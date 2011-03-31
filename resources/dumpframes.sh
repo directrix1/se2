4333ea3921f64a2c71d913679dfe08cdb7ec7ba8
Edward Flick <directrix1@gmail.com>
2011-03-31 00:00:08 -0500

    E time: 15 comment: Added bash script to flexibly generate test pngs and requisite xml file from flv video. Closes #8

--- resources/dumpframes.sh
+++ resources/dumpframes.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-ffmpeg -i rickroll.flv -r 10 -f image2 -s 160x130 rickrollframes/rickroll%05d.png
+mkdir rickrollframes
+ffmpeg -i rickroll.flv -r 29.97 -f image2 -s 160x130 rickrollframes/rickroll%05d.png
+cd rickrollframes
+export NUMFRAMES=`ls -1 | grep -c png`
+echo '<pnga frames="'$NUMFRAMES'" plays="1">' >> test.xml
+for i in *.png; do 
+	echo '<image src="rickrollframes/'$i'" length="100/2997"/>' >> test.xml
+done
+echo '</pnga>' >> test.xml
+
