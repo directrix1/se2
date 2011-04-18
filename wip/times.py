41c0822d8b852792b4d7139ef31ad0c8c4ea8f57
Edward Flick <directrix1@gmail.com>
2011-04-18 12:15:09 -0500

    E time: 15 comment: Updated design document and made time calculation program separate out by defect and total.

--- wip/times.py
+++ wip/times.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 import sys
 
+defecttime = 0
 totaltime = 0
 start = ""
 end = ""
 try:
     for a in sys.stdin.readlines():
         if "fix time:" in a:
-            totaltime += int(a.split(":")[1].strip())
+            defecttime += int(a.split(":")[1].strip())
         elif "start time:" in a:
             start = a.split(":",1)[1].strip().upper()
         elif "end time:" in a:
@@ -21,4 +22,6 @@ try:
                     (720 if sam!=eam else 0)
             totaltime+=end-start
 finally:
-    print totaltime
+    totaltime += defecttime
+    print "Defect fixing time: %i:%.2i" % (defecttime/60, defecttime % 60)
+    print "Total time: %i:%.2i" % (totaltime/60, totaltime % 60)
