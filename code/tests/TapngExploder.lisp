5728f04488539fcfd1659feb93b28c9b345a8d22
ZachBartlett <zachary.d.bartlett-1@ou.edu>
2011-04-18 00:07:07 -0500

    Z time: 25 comment: Added check-expects to TapngExploder.lisp --better than Builder, but still finding real data to check is difficult

--- code/tests/TapngExploder.lisp
+++ code/tests/TapngExploder.lisp
@@ -20,7 +20,96 @@
 
   (include-book "testing" :dir :teachpacks)
   (include-book "doublecheck" :dir :teachpacks)
-  (include-book "audio" :dir :teachpacks))
+  (include-book "audio" :dir :teachpacks)
+
+  (check-expect 
+    	(getFrames nil nil nil nil)
+	nil)
+  
+  (check-expect 
+    	(buildDataChunk nil nil nil)
+	(list nil nil))
+
+  (check-expect
+        (splitByFcTL nil nil)
+	(list nil nil))
+
+  (check-expect
+    	(splitByFcTL nil (list (list "IDAT" '(42))))
+	(list (list (list "IDAT" '(42))) nil))
+
+  (check-expect
+    	(splitByFcTL nil (list (list "IDAT" '(42)) (list "IEND" '(65))))
+	(list (list (list "IDAT" '(42))) (list (list "IEND" '(65)))))
+  
+  (check-expect
+        (splitByFcTL nil (list (list "IHDR" '(23)) (list "IDAT" '(8))
+			       (list "fcTL" '(4))  (list "fdAT" '(15))))
+	(list
+	  (list (list "IHDR" '(23)) (list "IDAT" '(8)))
+	  (list (list "fcTL" '(4))  (list "fdAT" '(15)))))
+
+  (check-expect
+      	(makeChunks nil)
+	nil)
+
+  (check-expect
+    	(splitAtFirstFrameChunk nil nil (list 
+				  (list "IHDR" '(23)) (list "IDAT" '(8))
+			          (list "fcTL" '(4)) (list "fdAT" '(15))))
+	 (list
+	  (list (list "IHDR" '(23)) (list "IDAT" '(8)))
+	  (list (list "fcTL" '(4))  (list "fdAT" '(15)))))
+ 
+  (check-expect
+    	(getIHDR (list (list "IHDR" '(42))))
+	'(42))
+  
+  (check-expect
+    	(getIHDR (list (list "fcTL" '(4))))
+	nil)
+
+  (check-expect
+    	(getIHDR nil)
+	nil)
+ 
+  (check-expect
+        (getChunksWithName "IHDR" (list (list "IHDR" '(42))
+					(list "fcTL" '(4))
+					(list "fdAT" '(8))
+					(list "IHDR" '(43))))
+	(list
+	  (list (list "IHDR" '(42)) (list "IHDR" '(43)))
+	  (list (list "fcTL" '(4)) (list "fdAT" '(8)))))
+
+  (check-expect
+    	(getChunksWithName "IHDR" (list (list "fcTL" '(4))))
+	(list
+	  nil
+	  (list (list "fcTL" '(4)))))
+
+  (check-expect
+    	(getChunksWithName "IHDR" nil)
+	(list
+	  nil
+	  nil))
+
+  (check-expect
+    	(takeChunk "IHDR" (list (list "IHDR" '(42))
+		(list "fcTL" '(4))
+		(list "fdAT" '(8))
+		(list "IHDR" '(43))))
+	(list
+	  (list "IHDR" '(42))
+	  (list	(list "fcTL" '(4))
+	        (list "fdAT" '(8))
+                (list "IHDR" '(43)))))
+
+  (check-expect
+    	(takeChunk "IHDR" (list (list "fcTL" '(4))))
+	nil)
+
+)
 
 (link Test
   (MpngUtils MapngExploder))
