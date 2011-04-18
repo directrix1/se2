7ca8debe0c57762ac421d96c0aa6a21daa7712d6
Micah Morgan <mnmorgan@ou.edu>
2011-04-18 14:43:47 -0500

    M time: 120 comment: Fixed a few check-expects, spent a lot of time messing around with actual data-using checks as well..

--- code/interfaces/IapngExploder.lisp
+++ code/interfaces/IapngExploder.lisp
@@ -1,11 +1,7 @@
 ;; The first four lines of this file were added by Dracula.
 ;; They tell DrScheme that this is a Dracula Modular ACL2 program.
 ;; Leave these lines unchanged so that DrScheme can properly load this file.
-#reader(planet "reader.ss" ("cce" "dracula.plt") "modular" "lang")
-;; The first four lines of this file were added by Dracula.
-;; They tell DrScheme that this is a Dracula Modular ACL2 program.
-;; Leave these lines unchanged so that DrScheme can properly load this file.
-;; #reader(planet "reader.rkt" ("cce" "dracula.plt") "modular" "lang")
+#reader(planet "reader.rkt" ("cce" "dracula.plt") "modular" "lang")
 #|
    Team Steele
    Software Engineering II
@@ -37,6 +33,47 @@
   ; the list described in (a) and the second is the list described in (b).
   (sig getChunksWithName (name chunkstocheck))
 
+  ; Given APNG chunks, returns the IHDR chunk contained within.
+  ; chunks = processed (or raw) data chunks contained within the input APNG
+  (sig getIHDR (chunks))
+  
+  ; Given APNG chunks, returns the numFrames and numPlays contained within
+  ; the acTL chunk following the IHDR chunk. These two elements completely
+  ;   comprise the acTL chunk.
+  ; chunks = processed (or raw) data chunks contained within the input APNG
+  (sig getacTL (chunks))
+  
+  ; Takes out the extra chunks that come before the next certain chunk
+  ; If the IDATflag is true, it stops at IDAT, fcTL, and IEND chunks
+  ; else it stops at only the fcTL or IEND chunks
+  (sig splitAtFirstFrameChunk (IDATflag pre post))
+  
+   ; Makes all the chunks in the list of chunks and concatenates them
+  (sig makeChunks (chunklist))
+  
+  ; Splits the chunklist on the next fcTL
+  (sig splitByFcTL (pre post))
+  
+  ; This function formats the raw PNG file data into more conveniently 
+  ; utilized chunks, and returns (list fdat IDAT) where IHDR is the IHDR
+  ; chunk IDAT is all IDAT chunks concatenated into one chunk.
+  ; fdat = fdat chunk data, pass in nil initially
+  ; idat = idat chunk data, pass in nil initially
+  ; chunks = a list of blown chunks
+  (sig buildDataChunk (fdat idat chunks))
+  
+  
+  ; Given the APNG chunks from input, the last found fcTL chunk, and the
+  ; IHDR chunk for the APNG, constructs from the next fdAT chunk the PNG
+  ; Signature, IHDR, IDAT, and IEND chunks necessary to create a complete
+  ; PNG file. 
+  ; chunks = processed (or raw) data chunks contained within the input APNG
+  ; IDATflag = Defines whether or not the IDAT chunks have been pulled yet
+  ; ihdr = ihdr for entire APNG file, data contained herein will be used to
+  ;   reconstruct all PNG files
+  (sig getFrame (chunks IDATflag prefix ihdr))
+  (sig getFrames (chunks IDATflag prefix ihdr))
+  
   ; Given an APNG file, breaks the APNG into its constituent PNG Images.
   ; This process involves looking at the acTL chunk to determine number of
   ; frames and number of plays, as well as looking at the fcTL and fdAT
@@ -50,28 +87,6 @@
   ; apngdata = raw apng data string given from the IO Module.
   ; Output is a list of lists: (file-data, file-name)
   (sig explodeAPNG (apngdata))
-
-  ; Given APNG chunks, returns the IHDR chunk contained within.
-  ; chunks = processed (or raw) data chunks contained within the input APNG
-  (sig getIHDR (chunks))
-  
-  ; Given APNG chunks, returns the numFrames and numPlays contained within
-  ; the acTL chunk following the IHDR chunk. These two elements completely
-  ;   comprise the acTL chunk.
-  ; chunks = processed (or raw) data chunks contained within the input APNG
-  (sig getacTL (chunks))
-  
-  ; Given the APNG chunks from input, the last found fcTL chunk, and the
-  ; IHDR chunk for the APNG, constructs from the next fdAT chunk the PNG
-  ; Signature, IHDR, IDAT, and IEND chunks necessary to create a complete
-  ; PNG file. 
-  ; chunks = processed (or raw) data chunks contained within the input APNG
-  ; IDATflag = Defines whether or not the IDAT chunks have been pulled yet
-  ; ihdr = ihdr for entire APNG file, data contained herein will be used to
-  ;   reconstruct all PNG files
-  (sig getFrames (chunks IDATflag prefix ihdr))
-  
-  
   
   ;;;;;;;;;;;;;;;;;;;;;;;; Contracts
   (con takeChunk-always-finds-first
--- code/tests/TapngExploder.lisp
+++ code/tests/TapngExploder.lisp
@@ -10,108 +10,156 @@
 |#
 (in-package "ACL2")
 
-(require "../interfaces/IapngExloder.lisp")
+(require "../interfaces/IapngExploder.lisp")
 (require "../interfaces/IpngUtils.lisp") ; XXX needed?
-(require "../modules/MapngExloder.lisp")
+(require "../modules/MapngExploder.lisp")
 (require "../modules/MpngUtils.lisp") ; XXX needed?
+
+
+
+(defconst *APNG* 
+  (list	(list "IHDR" (list 0 0 0 1 0 0 0 1 8 2 0 0 0))
+        (list "acTL" 
+              (list 0 0 0 3 0 0 0 0))
+        (list "fcTL" 
+              (list 0 0 0 0 0 0 0 1 0 0 0 1 0 0 0 0 0 0 0 0 0 1 0 1 0 0))
+        (list "IDAT" 
+              (list 120 218 98 98 96 96 0 8 48 0 0 12 0 3))
+        (list "fcTL" 
+              (list 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 0 0 0 0 0 0 1 0 1 0 0))
+        (list "fdAT" 
+              (list 0 0 0 2 120 218 98 98 96 96 0 8 48 0 0 12 0 3))
+        (list "fcTL" 
+              (list 0 0 0 3 0 0 0 1 0 0 0 1 0 0 0 0 0 0 0 0 0 1 0 1 0 0))
+        (list "fdAT" 
+              (list 0 0 0 4 120 218 98 98 96 96 0 8 48 0 0 12 0 3))
+        (list "IEND" 
+              nil)))
 
 (module TapngExloder
   (import IapngExploder)
-
+  
   (include-book "testing" :dir :teachpacks)
   (include-book "doublecheck" :dir :teachpacks)
   (include-book "audio" :dir :teachpacks)
-
-  (check-expect 
-    	(getFrames nil nil nil nil)
-	nil)
   
   (check-expect 
-    	(buildDataChunk nil nil nil)
-	(list nil nil))
-
-  (check-expect
-        (splitByFcTL nil nil)
-	(list nil nil))
-
-  (check-expect
-    	(splitByFcTL nil (list (list "IDAT" '(42))))
-	(list (list (list "IDAT" '(42))) nil))
-
-  (check-expect
-    	(splitByFcTL nil (list (list "IDAT" '(42)) (list "IEND" '(65))))
-	(list (list (list "IDAT" '(42))) (list (list "IEND" '(65)))))
+    (getFrames nil nil nil nil)
+    nil)
+  
+  (check-expect 
+    (buildDataChunk nil nil nil)
+    (list nil nil))
   
   (check-expect
-        (splitByFcTL nil (list (list "IHDR" '(23)) (list "IDAT" '(8))
-			       (list "fcTL" '(4))  (list "fdAT" '(15))))
-	(list
-	  (list (list "IHDR" '(23)) (list "IDAT" '(8)))
-	  (list (list "fcTL" '(4))  (list "fdAT" '(15)))))
-
-  (check-expect
-      	(makeChunks nil)
-	nil)
-
-  (check-expect
-    	(splitAtFirstFrameChunk nil nil (list 
-				  (list "IHDR" '(23)) (list "IDAT" '(8))
-			          (list "fcTL" '(4)) (list "fdAT" '(15))))
-	 (list
-	  (list (list "IHDR" '(23)) (list "IDAT" '(8)))
-	  (list (list "fcTL" '(4))  (list "fdAT" '(15)))))
- 
-  (check-expect
-    	(getIHDR (list (list "IHDR" '(42))))
-	'(42))
+    (splitByFcTL nil nil)
+    (list nil nil))
   
   (check-expect
-    	(getIHDR (list (list "fcTL" '(4))))
-	nil)
-
+    (splitByFcTL nil (list (list "IDAT" '(42))))
+    (list (list (list "IDAT" '(42))) nil))
+  
   (check-expect
-    	(getIHDR nil)
-	nil)
- 
+    (splitByFcTL nil (list (list "IDAT" '(42)) (list "IEND" '(65))))
+    (list (list (list "IDAT" '(42))) (list (list "IEND" '(65)))))
+  
   (check-expect
-        (getChunksWithName "IHDR" (list (list "IHDR" '(42))
-					(list "fcTL" '(4))
-					(list "fdAT" '(8))
-					(list "IHDR" '(43))))
-	(list
-	  (list (list "IHDR" '(42)) (list "IHDR" '(43)))
-	  (list (list "fcTL" '(4)) (list "fdAT" '(8)))))
-
+    (splitByFcTL nil (list (list "IHDR" '(23)) (list "IDAT" '(8))
+                           (list "fcTL" '(4))  (list "fdAT" '(15))))
+   (list
+     (list (list "IHDR" '(23)) (list "IDAT" '(8)))
+     (list (list "fcTL" '(4))  (list "fdAT" '(15)))))
+  
   (check-expect
-    	(getChunksWithName "IHDR" (list (list "fcTL" '(4))))
-	(list
-	  nil
-	  (list (list "fcTL" '(4)))))
-
+    (makeChunks nil)
+    nil)
+  
   (check-expect
-    	(getChunksWithName "IHDR" nil)
-	(list
-	  nil
-	  nil))
-
+    (splitAtFirstFrameChunk nil nil 
+                            (list 
+                              (list "IHDR" '(23)) (list "IDAT" '(8))
+                              (list "fcTL" '(4)) (list "fdAT" '(15))))
+    (list
+      (list (list "IHDR" '(23)) (list "IDAT" '(8)))
+      (list (list "fcTL" '(4))  (list "fdAT" '(15)))))
+  
   (check-expect
-    	(takeChunk "IHDR" (list (list "IHDR" '(42))
-		(list "fcTL" '(4))
-		(list "fdAT" '(8))
-		(list "IHDR" '(43))))
-	(list
-	  (list "IHDR" '(42))
-	  (list	(list "fcTL" '(4))
-	        (list "fdAT" '(8))
-                (list "IHDR" '(43)))))
-
+    (getIHDR (list (list "IHDR" '(42))))
+    '(42))
+  
   (check-expect
-    	(takeChunk "IHDR" (list (list "fcTL" '(4))))
-	nil)
-
-)
+    (getIHDR (list (list "fcTL" '(4))))
+    nil)
+  
+  (check-expect
+    (getIHDR nil)
+    nil)
+  
+  (check-expect
+    (getChunksWithName "IHDR" (list (list "IHDR" '(42))
+                                    (list "fcTL" '(4))
+                                    (list "fdAT" '(8))
+                                    (list "IHDR" '(43))))
+    (list
+      (list (list "IHDR" '(42)) (list "IHDR" '(43)))
+      (list (list "fcTL" '(4)) (list "fdAT" '(8)))))
+  
+  (check-expect
+    (getChunksWithName "IHDR" (list (list "fcTL" '(4))))
+    (list
+      nil
+      (list (list "fcTL" '(4)))))
+  
+  (check-expect
+    (getChunksWithName "IHDR" nil)
+    (list
+      nil
+      nil))
+  
+  (check-expect
+    (takeChunk "IHDR" (list (list "IHDR" '(42))
+                            (list "fcTL" '(4))
+                            (list "fdAT" '(8))
+                            (list "IHDR" '(43)))
+               nil)
+    (list
+      (list "IHDR" '(42))
+      (list	  (list "fcTL" '(4))
+                  (list "fdAT" '(8))
+                  (list "IHDR" '(43)))))
+  
+  (check-expect
+    (takeChunk "IHDR" (list (list "fcTL" '(4))) nil)
+    nil)
+  
+  (check-expect
+    (explodeAPNG (makeChunks *APNG*))
+    (list "3" "0" (list 
+                   (list (list 137 80 78 71 13 10 26 10 0 0 0 13 73 72 68
+                               82 0 0 0 1 0 0 0 1 8 2 0 0 0 144 119 83 222
+                               0 0 0 14 73 68 65 84 120 218 98 98 96 96 0
+                               8 48 0 0 12 0 3 35 114 99 92 0 0 0 26 102
+                               99 84 76 0 0 0 0 0 0 0 1 0 0 0 1 0 0 0 0 0
+                               0 0 0 0 1 0 1 0 0 86 42 223 49 0 0 0 0 73 
+                               69 78 68 174 66 96 130) "1/1") 
+                   (list (list 137 80 78 71 13 10 26 10 0 0 0 13 73 72 68
+                               82 0 0 0 1 0 0 0 1 8 2 0 0 0 144 119 83 222
+                               0 0 0 14 73 68 65 84 120 218 98 98 96 96 0
+                               8 48 0 0 12 0 3 35 114 99 92 0 0 0 26 102
+                               99 84 76 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 0 
+                               0 0 0 0 0 1 0 1 0 0 205 89 53 229 0 0 0 0
+                               73 69 78 68 174 66 96 130) "1/1")
+                   (list (list 137 80 78 71 13 10 26 10 0 0 0 13 73 72 68
+                               82 0 0 0 1 0 0 0 1 8 2 0 0 0 144 119 83 222
+                               0 0 0 14 73 68 65 84 120 218 98 98 96 96 0
+                               8 48 0 0 12 0 3 35 114 99 92 0 0 0 26 102
+                               99 84 76 0 0 0 3 0 0 0 1 0 0 0 1 0 0 0 0
+                               0 0 0 0 0 1 0 1 0 0 32 207 230 12 0 0 0 0
+                               73 69 78 68 174 66 96 130) "1/1"))))
+  )
 
 (link Test
-  (MpngUtils MapngExploder))
+      (MpngUtils MapngExploder))
 
 (invoke Test)
+
